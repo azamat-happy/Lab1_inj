87c9f8227e9236ce512f471eeddc7bd468edf1ee
Азамат Шарипов <azamatfootball1003@outlook.com>
2022-03-19 15:56:18 +0500

    Добавьте файлы проекта.

---
+++ Lab1_inj/Lab1_inj.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "math_3d.h"
+#include "glm/glm.hpp"
+
+
+
+
+int main(int argc, char** argv)
+{
+   
+
+    return 0;
+}
