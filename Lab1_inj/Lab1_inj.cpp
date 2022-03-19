6359dbe17d117dd845ada6b69f36bb58c12ca6cd
Азамат Шарипов <azamatfootball1003@outlook.com>
2022-03-19 16:07:27 +0500

    2 урок. Рисование точки. Добавление функции создания вектора из точки

--- Lab1_inj/Lab1_inj.cpp
+++ Lab1_inj/Lab1_inj.cpp
@@ -9,7 +9,25 @@
 GLuint VBO;
 static void RenderSceneCB() {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
+}
+static void CreateVertexBuffer()
+{
+    glm::vec3 Vertices[1];
+    Vertices[0] = glm::vec3(0.7f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 int main(int argc, char** argv)
 {
@@ -17,10 +35,21 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Tutorial 02");
+
     glutDisplayFunc(RenderSceneCB);
-    glClearColor(0.6f, 0.0f, 0.0f, 0.0f);
+
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
 
     glutMainLoop();
+
     return 0;
 }
