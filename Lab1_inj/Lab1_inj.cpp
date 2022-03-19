5aee9c72cfe6b8394d79ddbc217b35b03f1c12af
Азамат Шарипов <azamatfootball1003@outlook.com>
2022-03-19 16:03:37 +0500

    1 урок. Создание экрана

--- Lab1_inj/Lab1_inj.cpp
+++ Lab1_inj/Lab1_inj.cpp
@@ -1,15 +1,26 @@
 ﻿#include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include "math_3d.h"
 #include "glm/glm.hpp"
 
 
 
-
+// Хранит указатель на буфер вершин
+GLuint VBO;
+static void RenderSceneCB() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
 int main(int argc, char** argv)
 {
-   
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 01");
+    glutDisplayFunc(RenderSceneCB);
+    glClearColor(0.6f, 0.0f, 0.0f, 0.0f);
 
+    glutMainLoop();
     return 0;
 }
