8e8e99ea2cbb3db343cb8a56b917afd523054bf7
Азамат Шарипов <azamatfootball1003@outlook.com>
2022-04-08 18:02:45 +0500

    преобразование масштаба

--- Lab1_inj/Lab1_inj.cpp
+++ Lab1_inj/Lab1_inj.cpp
@@ -58,6 +58,12 @@ static void RenderSceneCB() {
 	//World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
 	//World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
 
+	//преобразование масштаба
+    World[0][0]=sinf(Scale); World[0][1]=0.0f;        World[0][2]=0.0f;        World[0][3]=0.0f;
+	World[1][0] = 0.0f;        World[1][1] = cosf(Scale); World[1][2] = 0.0f;        World[1][3] = 0.0f;
+	World[2][0] = 0.0f;        World[2][1] = 0.0f;        World[2][2] = sinf(Scale); World[2][3] = 0.0f;
+	World[3][0] = 0.0f;        World[3][1] = 0.0f;        World[3][2] = 0.0f;        World[3][3] = 1.0f;
+
 	//Наш третий параметр в glUniformMatrix4fv() - это GL_TRUE, потому что мы поставляем матрицу упорядоченную по строкам.
 	//Четвертый параметр - это просто указатель на первый элемент матрицы.
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
